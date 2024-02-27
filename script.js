88b192dec62db4513acd812902e80bc8fb3d7978
Ozioma45 <oziomaegole@gmail.com>
2024-02-27 18:12:52 +0100

    implement the Observer Pattern

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 class Telephone {
+  constructor() {
+    this.observers = new Set();
+  }
+
+  add(observer) {
+    this.observers.add(observer);
+  }
+
+  remove(observer) {
+    this.observers.delete(observer);
+  }
+
+  notify(context) {
+    for (let observer of this.observers) {
+      observer.update(context);
+    }
+  }
+
   AddPhoneNumber() {}
 
   RemovePhoneNumber() {}
